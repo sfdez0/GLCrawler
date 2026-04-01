5a02666eb60bb362f7bdfd3caa33396e87090425
Sergio Fernández <sergiofdez2000@gmail.com>
2026-04-01 12:57:48 +0200

    Creación de escenario (prisma rectangular), activado z-buffer y cámara básica

--- src/GPO_proyecto_final.cpp
+++ src/GPO_proyecto_final.cpp
@@ -43,59 +43,129 @@ void main()
 
 GLFWwindow* window;
 GLuint prog;
-objeto triangulo;
+objeto escena_cubica; // Objeto para el escenario del laberinto
 
-objeto crear_triangulo(void)
+// Función que crea un rectángulo 3D (escenario del laberinto)
+// Dimensiones: Ancho(X) 10, Alto(Y) 4, Profundidad(Z) 20
+objeto crear_escena_cubica(void)
 {
 	objeto obj;
 	GLuint VAO;
 	GLuint buffer_pos, buffer_col;
 
-	GLfloat pos_data[3][3] = { 0.0f,  0.0000f,  1.0f,  // Posici�n vertice 1
-							   0.0f, -0.8660f, -0.5f,  // Posici�n vertice 2
-							   0.0f,  0.8660f, -0.5f}; // Posici�n vertice 3
+	// Definimos los 36 vértices del cubo (6 caras x 2 tríangulos x 3 vértices)
+	// Dimensiones: X(-5 a 5), Y(-2 a 2), Z(-10 a 10)
+	GLfloat pos_data[36][3] = {
+		// Cara frontal (z = 10) - Rojo
+		-5.0f, -2.0f,  10.0f,   // tri 1: inf-izq
+		 5.0f, -2.0f,  10.0f,   //        inf-der
+		 5.0f,  2.0f,  10.0f,   //        sup-der
+		-5.0f, -2.0f,  10.0f,   // tri 2: inf-izq
+		 5.0f,  2.0f,  10.0f,   //        sup-der
+		-5.0f,  2.0f,  10.0f,   //        sup-izq
 
-	GLfloat color_data[3][3] = { 1.0f, 0.0f, 0.0f,  // Color vertice 1
-		                         0.0f, 1.0f, 0.0f,  // Color vertice 2 
-								 0.0f, 0.0f, 1.0f }; // Color vertice 3
+		// Cara trasera (z = -10) - Verde
+		 5.0f, -2.0f, -10.0f,   // tri 1: inf-der
+		-5.0f, -2.0f, -10.0f,   //        inf-izq
+		-5.0f,  2.0f, -10.0f,   //        sup-izq
+		 5.0f, -2.0f, -10.0f,   // tri 2: inf-der
+		-5.0f,  2.0f, -10.0f,   //        sup-izq
+		 5.0f,  2.0f, -10.0f,   //        sup-der
 
-	// Mando posiciones en un VBO
+		// Cara derecha (x = 5) - 2 Azul
+		 5.0f, -2.0f,  10.0f,   // tri 1: inf-der
+		 5.0f, -2.0f, -10.0f,   //        inf-izq
+		 5.0f,  2.0f, -10.0f,   //        sup-izq
+		 5.0f, -2.0f,  10.0f,   // tri 2: inf-der
+		 5.0f,  2.0f, -10.0f,   //        sup-izq
+		 5.0f,  2.0f,  10.0f,   //        sup-der
+
+		// Cara izquierda (x = -5) - Amarillo
+		-5.0f, -2.0f, -10.0f,   // tri 1: inf-der
+		-5.0f, -2.0f,  10.0f,   //        inf-izq
+		-5.0f,  2.0f,  10.0f,   //        sup-izq
+		-5.0f, -2.0f, -10.0f,   // tri 2: inf-der
+		-5.0f,  2.0f,  10.0f,   //        sup-izq
+		-5.0f,  2.0f, -10.0f,   //        sup-der
+
+		// Cara superior (y = 2) - Cian
+		-5.0f,  2.0f,  10.0f,   // tri 1: izq-atras
+		 5.0f,  2.0f,  10.0f,   //        der-atras
+		 5.0f,  2.0f, -10.0f,   //        der-frente
+		-5.0f,  2.0f,  10.0f,   // tri 2: izq-atras
+		 5.0f,  2.0f, -10.0f,   //        der-frente
+		-5.0f,  2.0f, -10.0f,   //        izq-frente
+
+		// Cara inferior (y = -2) - Magenta
+		-5.0f, -2.0f, -10.0f,   // tri 1: izq-frente
+		 5.0f, -2.0f, -10.0f,   //        der-frente
+		 5.0f, -2.0f,  10.0f,   //        der-atras
+		-5.0f, -2.0f, -10.0f,   // tri 2: izq-frente
+		 5.0f, -2.0f,  10.0f,   //        der-atras
+		-5.0f, -2.0f,  10.0f    //        izq-atras
+	};
+
+	// Colores para cada cara
+	GLfloat color_data[36][3];
+	
+	// Definimos los colores para cada cara
+	GLfloat colors[6][3] = {
+		{1.0f, 0.0f, 0.0f},  // Rojo (frontal)
+		{0.0f, 1.0f, 0.0f},  // Verde (trasera)
+		{0.0f, 0.0f, 1.0f},  // Azul (derecha)
+		{1.0f, 1.0f, 0.0f},  // Amarillo (izquierda)
+		{0.0f, 1.0f, 1.0f},  // Cian (superior/techo)
+		{1.0f, 0.0f, 1.0f}   // Magenta (inferior/suelo)
+	};
+	
+	// Bucle para asignar colores a todos los vértices
+	for (int i = 0; i < 36; i++) {
+		// Determinamos a que cara pertenece el vértice
+		int face = i / 6; 
+
+		// Aplicamos el color RGB
+		color_data[i][0] = colors[face][0];
+		color_data[i][1] = colors[face][1];
+		color_data[i][2] = colors[face][2];
+	}
+
+	// Mandamos posiciones en un VBO
 	glGenBuffers(1, &buffer_pos); glBindBuffer(GL_ARRAY_BUFFER, buffer_pos);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(pos_data), pos_data, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	// Mando colores en otro VBO
+	// Mandamos colores en otro VBO
 	glGenBuffers(1, &buffer_col); glBindBuffer(GL_ARRAY_BUFFER, buffer_col);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(color_data), color_data, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+	// Creamos y enlazamos el VAO
+	glGenVertexArrays(1, &VAO);	
+	glBindVertexArray(VAO);
 
-	// Creo y enlazo el VAO
-	glGenVertexArrays(1, &VAO);	glBindVertexArray(VAO);
-
-	// Indico donde hallar datos de posiciones dentro del VBO correspondiente
+	// Indicamos donde hallar datos de posiciones
 	glBindBuffer(GL_ARRAY_BUFFER, buffer_pos);
-	glEnableVertexAttribArray(0);  // Organizaci�n de los datos del atributo 0 (pos) del vertex shade
+	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	// Indico donde hallar datos de colores dentro del VBO correspondiente
+	// Indicamos donde hallar datos de colores
 	glBindBuffer(GL_ARRAY_BUFFER, buffer_col);
-	glEnableVertexAttribArray(1);  // Organizaci�n de los datos del atributo 0 (pos) del vertex shade
+	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	glBindVertexArray(0);  //Cerramos VAO con todo listo para ser pintado
+	// Cerramos VAO con todo listo para ser pintado
+	glBindVertexArray(0);
 
-	obj.VAO = VAO; obj.Nv = 3;  // Devuelvo objeto VAO + n�mero de vertices en estructura obj
-
+	// Devolvemos objeto VAO + número de vértices en estructura obj
+	obj.VAO = VAO; 
+	obj.Nv = 36;
 	return obj;
-
 }
 
-
-// Preparaci�n de los datos de los objetos a dibujar, envialarlos a la GPU
-// Compilaci�n programas a ejecutar en la tarjeta gr�fica:  vertex shader, fragment shaders
+// Preparacion de los datos de los objetos a dibujar, envialarlos a la GPU
+// Compilacion programas a ejecutar en la tarjeta grafica:  vertex shader, fragment shaders
 // Opciones generales de render de OpenGL
 void init_scene()
 {
@@ -103,64 +173,74 @@ void init_scene()
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height); 
     
-	triangulo = crear_triangulo();  // Preparar datos de objeto, mandar a GPU
+	escena_cubica = crear_escena_cubica();  // Crear el escenario cubico
 
-	// Mandar programas a GPU, compilar y crear programa en GPU
+	// Habilitamos test de profundidad para renderizar correctamente las caras
+	glEnable(GL_DEPTH_TEST);
 
-	// Compilear Shaders
+	// Mandamos programas a GPU, compilar y crear programa en GPU
+
+	// Compilar Shaders
 	GLuint VertexShaderID = compilar_shader(vertex_prog, GL_VERTEX_SHADER);
 	GLuint FragmentShaderID = compilar_shader(fragment_prog, GL_FRAGMENT_SHADER);
 
 	// Enlazar sharders en el programa final
 	prog = glCreateProgram();
-	glAttachShader(prog, VertexShaderID);  glAttachShader(prog, FragmentShaderID);
-	glLinkProgram(prog); check_errores_programa(prog);
+	glAttachShader(prog, VertexShaderID);  
+	glAttachShader(prog, FragmentShaderID);
+	glLinkProgram(prog); 
+	check_errores_programa(prog);
 
 	// Limpieza final de los shaders una vez compilado el programa
-	glDetachShader(prog, VertexShaderID);  glDeleteShader(VertexShaderID);
-	glDetachShader(prog, FragmentShaderID);  glDeleteShader(FragmentShaderID);
+	glDetachShader(prog, VertexShaderID);  
+	glDeleteShader(VertexShaderID);
+	glDetachShader(prog, FragmentShaderID);  
+	glDeleteShader(FragmentShaderID);
 
 	// Alternativamente usar la funci�n Compile_Link_Shaders().
 	//	prog = Compile_Link_Shaders(vertex_prog, fragment_prog); 
 
-	glUseProgram(prog);    // Indicamos que programa vamos a usar 
+	// Indicamos que programa vamos a usar 
+	glUseProgram(prog);
 }
 
 
-vec3 pos_obs=vec3(10.0f,0.0f,0.0f); //###vec3 pos_obs=vec3(1.5f,0.0f,0.0f); 
-vec3 target = vec3(0.0f,0.0f,0.0f);
-vec3 up = vec3(0,0,1);
+vec3 pos_obs = vec3(0.0f, 0.0f, 0.0f); // Posición inicial de la cámara (observador)
+vec3 front = vec3(0.0f, 0.0f, 1.0f); // Dirección hacia donde mira la cámara
+vec3 up = vec3(0.0f, 1.0f, 0.0f); // Vector "arriba" de la cámara
 
-float fov = 35.0f, aspect = 4.0f / 3.0f; //###float fov = 40.0f, aspect = 4.0f / 3.0f;
+float fov = 60.0f; // Campo de visión inicial
+float aspect = 4.0f / 3.0f; // Aspect ratio inicial (proporción de la ventana)
+float speed = 0.2f;  // Velocidad de movimiento de la cámara
 
-// Actualizar escena: cambiar posici�n objetos, nuevos objetros, posici�n c�mara, luces, etc.
+// Actualizar escena: cambiar posicion objetos, nuevos objetros, posicion camara, luces, etc.
 void render_scene()
 {
-	glClearColor(0.0f,0.0f,0.0f,1.0f);  // Especifica color para el fondo (RGB+alfa)
-	glClear(GL_COLOR_BUFFER_BIT);          // Aplica color asignado borrando el buffer
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);  // Especifica color para el fondo oscuro (RGB+alfa)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  // Limpia buffers de color y profundidad
 
 	float t = (float)glfwGetTime();  // Contador de tiempo en segundos 
 
-
 	///////// Actualizacion matrices M, V, P  /////////	
-	mat4 P,V,M,T,R,S;
+	mat4 P, V, M, T, R, S;
 
-	P = perspective(glm::radians(fov), aspect, 0.5f, 20.0f);  //40� FOV,  4:3 ,  Znear=0.5, Zfar=20
-	V = lookAt(pos_obs, target, up  );  // Pos camara, Lookat, head up
+	P = perspective(glm::radians(fov), aspect, 0.1f, 50.0f);  // FOV, aspect ratio, Znear, Zfar
 	
-	//T = translate(0.0f, 0.0f, 3.0f*sin(t));  
-	T = glm::translate(glm::vec3(0.0, 0.0, 3.0f*sin(t))); 
+	// El target es un punto adelante de la cámara en la dirección donde mira
+	vec3 target = pos_obs + front;
+	V = lookAt(pos_obs, target, up);  // Pos camara, Lookat relativo a dirección, head up
 	
-	M = T;
-	transfer_mat4("MVP",P*V*M);
+	// Matriz identidad para el cubo (sin transformaciones)
+	M = glm::mat4(1.0f);
 	
-	// ORDEN de dibujar
-	glBindVertexArray(triangulo.VAO);              // Activamos VAO asociado al objeto
-    glDrawArrays(GL_TRIANGLES, 0, triangulo.Nv);   // Orden de dibujar (Nv vertices)	
-	glBindVertexArray(0);                          // Desconectamos VAO
+	transfer_mat4("MVP", P * V * M);
+	
+	// Dibujamos el cubo del escenario
+	glBindVertexArray(escena_cubica.VAO);             // Activamos VAO del cubo
+	glDrawArrays(GL_TRIANGLES, 0, escena_cubica.Nv);  // Dibujamos todos los triangulos del cubo
+	glBindVertexArray(0);                             // Desconectamos VAO
 
 	////////////////////////////////////////////////////////
-
 }
 
 
@@ -174,7 +254,9 @@ int main(int argc, char* argv[])
 	load_Opengl();         // Carga funciones de OpenGL, comprueba versi�n.
 	init_scene();          // Prepara escena
 	
+	// Hacemos que se sincronice con la tasa de refresco del monitor
 	glfwSwapInterval(1);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		render_scene();
