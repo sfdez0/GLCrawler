2f5fae1c46d1c00283825152b5d213545beb3ac3
Sergio Fernández <sergiofdez2000@gmail.com>
2026-05-08 16:56:05 +0200

    Corregido error en el uso de texturas de la puerta (generaba errores en el compilador de nvidia)

--- src/door.cpp
+++ src/door.cpp
@@ -132,13 +132,13 @@ namespace door {
 
                     // Especular 
                     vec3 R = reflect(-L, N);
-                    float specular = pow(max(dot(V, R), 0.0), specularPower) * 0.045;
+                    float specular = pow(max(dot(V, R), 0.0), shininess) * Ks;
 
                     // Calculos relativos a la luz del personaje
                     float light_cutoff = 1.0 - smoothstep(light_range - light_soft, light_range, light_dist); // Factor de atenuación basado en la distancia
                     float light_attenuation = light_cutoff / (1.0 + 0.2 * light_dist + 0.5 * light_dist * light_dist); // Atenuación
 
-                    vec3 contribution = (baseColor * diffuse * light_attenuation * ao) + vec3(specular * light_attenuation);
+                    vec3 contribution = (diffuseColor * diffuse * light_attenuation * ao) + (specular * specTint * light_attenuation);
 
                     // Aplicamos color e intensidad de la luz (incluye el parpadeo)
 			        result += contribution * lightColors[i] * lightIntensities[i];
